cf4be371d7682c802a8b77c0a19432f0f6e83ffe
Matt Vincent <mvincent@jax.org>
2022-02-04 08:28:25 -0500

    added tech.py for tech details

---
+++ qtlweb/tech.py
@@ -0,0 +1,92 @@
+TECHNOLOGIES = [
+  {
+    "category": "html",
+    "name": "Bootstrap",
+    "version": "4.3.1",
+    "url": "https://getbootstrap.com/"
+  },
+  {
+    "category": "html",
+    "name": "jQuery",
+    "version": "3.4.0",
+    "url": "https://jquery.com/"
+  },
+  {
+    "category": "html",
+    "name": "GenomeSpy",
+    "version": "0.17.0",
+    "url": "https://genomespy.app/"
+  },
+  {
+    "category": "html",
+    "name": "HighCharts",
+    "version": "8.0.4",
+    "url": "https://www.highcharts.com/"
+  },
+  {
+    "category": "python",
+    "name": "Python",
+    "version": 3.6,
+    "url": "https://www.python.org/"
+  },
+  {
+    "category": "python",
+    "name": "Flask",
+    "version": "1.0.2",
+    "url": "https://flask.palletsprojects.com/"
+  },
+  {
+    "category": "python",
+    "name": "Celery",
+    "version": "4.3.0",
+    "url": "https://docs.celeryproject.org/en/stable/"
+  },
+  {
+    "category": "R",
+    "name": "R",
+    "version": 4.1,
+    "url": "https://www.r-project.org/"
+  },
+  {
+    "category": "R",
+    "name": "R/qtl2",
+    "version": 0.28,
+    "url": "https://kbroman.org/qtl2/"
+  },
+  {
+    "category": "R",
+    "name": "qtl2api",
+    "version": "0.1.0",
+    "url": "https://github.com/churchill-lab/qtl2api"
+  },
+  {
+    "category": "R",
+    "name": "RestRserve",
+    "version": "0.4.1",
+    "url": "https://restrserve.org/"
+  },
+  {
+    "category": "docker",
+    "name": "Docker",
+    "version": "18.09.3",
+    "url": "https://www.docker.com/"
+  },
+  {
+    "category": "docker",
+    "name": "rocker/r-ver",
+    "version": 4.1,
+    "url": "https://hub.docker.com/r/rocker/r-ver"
+  },
+  {
+    "category": "docker",
+    "name": "mattjvincent/qtl2rest",
+    "version": "0.1.0",
+    "url": "https://github.com/churchill-lab/qtl2rest"
+  },
+  {
+    "category": "docker",
+    "name": "redis:4.0.11-alpine",
+    "version": "4.0.11",
+    "url": "https://hub.docker.com/_/redis"
+  }
+]
